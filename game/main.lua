825ee501cbd8bc3989fef1701071bb55cb8ecbc7
Tobias Gärdebro <tubbeg@gmail.com>
2025-10-30 20:07:32 +0100

    Added draw system

--- game/main.lua
+++ game/main.lua
@@ -1,4 +1,3 @@
-Board = require "board"
 Tiny = require "tiny-ecs/tiny"
 
 local WIDTH = 800
@@ -10,7 +9,7 @@ for i,v in pairs(List) do
     print(v)
 end
 
-local world = Tiny.world()
+local w = Tiny.world()
 local logicSystem = Tiny.requireAll("logicSystem")
 local graphicSystem = Tiny.rejectAll("logicSystem")
 
@@ -21,6 +20,7 @@ just a silly sideproject
 nothing serious
 just for fun :) 
 ]]
+
 
 --[[
 Prioritize getting a working prototype.
@@ -34,7 +34,18 @@ For now
 ]]
 
 
-local function addSystems(world)
+local function addDrawSystem(world)
+    local moveSystem = Tiny.processingSystem()
+    moveSystem.graphicSystem = true
+    moveSystem.filter = Tiny.requireAll("player", "position", "direction",  "image")
+    function moveSystem:process(e, dt)
+        love.graphics.draw(e.image, e.position.x, e.position.y)
+    end
+    Tiny.addSystem(world, moveSystem)
+end
+
+
+local function addMoveSystem(world)
     local moveSystem = Tiny.processingSystem()
     moveSystem.logicSystem = true
     moveSystem.filter = Tiny.requireAll("player", "position", "direction")
@@ -46,10 +57,23 @@ local function addSystems(world)
 end
 
 
+local function addSystems(world)
+    addMoveSystem(world)
+    addDrawSystem(world)
+end
+
+
 local function addPlayerEntity(world)
     local position = {x=10,y=50}
     local direction = {hello="there"}
-    local entity = {player=true, position=position, direction=direction}
+    local image = love.graphics.newImage("assets/banana.png")
+    local entity = 
+        {
+            image=image,
+            player=true,
+            position=position,
+            direction=direction
+        }
     Tiny.addEntity(world, entity)
 end
 
@@ -57,13 +81,12 @@ end
 function love.load()
     love.window.setMode( WIDTH, HEIGHT)
     love.graphics.setDefaultFilter("nearest", "nearest")
-    addSystems(world)
-    addPlayerEntity(world)
-    --Images.banana = love.graphics.newImage("assets/banana.png")
+    addSystems(w)
+    addPlayerEntity(w)
 end
 
 function love.keypressed(key)
-    for i,entity in ipairs(world.entities) do
+    for i,entity in ipairs(w.entities) do
         if entity.player then
             if key == "a" then
                 if entity then
@@ -74,11 +97,6 @@ function love.keypressed(key)
     end
 end
 
-function love.update(dt)
-    world:update(dt, logicSystem)
-end
+function love.update(dt) w:update(dt, logicSystem) end
 
-function love.draw()
-    world:update(love.timer.getDelta(), graphicSystem)
-    --love.graphics.draw(Images.banana, 300, 200, 0, 3,3)
-end
+function love.draw() w:update(love.timer.getDelta(), graphicSystem) end
